18fe0328dad9a94743f94c914a2580533f8e3512
Dean Harland <mrDeanHarland@gmail.com>
2026-02-03 16:14:37 +0000

    prepare for Heroku deployment with Procfile, runtime.txt, and production settings

--- joblinker/settings.py
+++ joblinker/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 from dotenv import load_dotenv
 import os
-
+import dj_database_url
 load_dotenv()
 
 from pathlib import Path
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("SECRET_KEY")
+SECRET_KEY = os.getenv("SECRET_KEY", "fallback-secret-key-for-local")
 DEBUG = os.getenv("DEBUG") == "True"
 
 
