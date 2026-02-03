9459e2ec4a430588c140c0d102e7879fe8b970a4
Dean Harland <mrDeanHarland@gmail.com>
2026-02-03 14:56:02 +0000

    Register Job and Application models n dhango admin with list display

--- jobs/admin.py
+++ jobs/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Job, Application
 
-# Register your models here.
+
+@admin.register(Job)
+class JobAdmin(admin.ModelAdmin):
+    list_display = ['title', 'company', 'location', 'salary', 'created_at']
+    list_filter = ['created_at', 'location']
+    search_fields = ['title', 'company', 'description']
+    readonly_fields = ['created_at']
+
+
+@admin.register(Application)
+class ApplicationAdmin(admin.ModelAdmin):
+    list_display = ['applicant', 'job', 'status', 'created_at']
+    list_filter = ['status', 'created_at']
+    search_fields = ['applicant__username', 'job__title']
+    readonly_fields = ['created_at']
